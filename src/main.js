efc35c9fa27cfa60694de2fd85d15f973ff60fe8
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 11:46:05 -0400

    remove default stylings

--- src/main.js
+++ src/main.js
@@ -2,12 +2,34 @@ import "./style.css";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <div>
+<header>
+      <div class="container">
+        <div class="logo">
+          <p>Josh Kaplan</p>
+        </div>
+        <nav class="nav">
+          <ul class="nav__list nav__list--primary">
+            <li class="nav__item"><a href="#" class="nav__link">Home</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">About</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+          </ul>
+          <ul class="nav__list nav__list--secondary">
+            <li class="nav__item">
+              <a href="#" class="nav__link">Sign In</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link nav__link--button">Sign up</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+<main>
     <h1>Welcome</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-  </div>
+</main>
 `;
 
 setupCounter(document.querySelector("#counter"));
