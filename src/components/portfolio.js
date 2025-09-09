e6fc998af1ecbdeda8eb03ea875250e61de48760
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 14:59:09 -0400

    make card clickable

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,6 @@
 import "../styles/portfolio.css";
 import placeholderImg from "../assets/images/placeholder_screenshot.png";
+import weatherApplicationImg from "../assets/images/weather-application_screenshot.png";
 
 export function makePortfolio(element) {
   element.innerHTML = `
@@ -7,10 +8,10 @@ export function makePortfolio(element) {
         <h2>Portfolio</h2>
         <div class="card-container">
             ${makeProjectCard(
-              placeholderImg,
-              "project screenshot",
-              "Project Title",
-              "Project description. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, tempora."
+              weatherApplicationImg,
+              "weather application screenshot",
+              "Weather Now",
+              "Weather application using the Open-Meteo API."
             )}
             ${makeProjectCard(
               placeholderImg,
@@ -38,5 +39,6 @@ function makeProjectCard(imgUrl, imgAltText, title, description) {
                 <h3 class="project-title">${title}</h3>
                 <p class="project-description">${description}</p>
             </div>
+            <a href="https://jkaps9.github.io/weather-application/" target="_blank" class="project-link"></a>
         </div>`;
 }
