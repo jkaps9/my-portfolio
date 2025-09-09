7aea1db06c012303dad52c80e799ed8b4eaff1f1
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 16:59:08 -0400

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import "../styles/footer.css";
+
+export function makeFooter(element) {
+  element.innerHTML = `
+    <div class="container">
+      <p>&copy; 2025 Josh F Kaplan</p>    
+    </div>
+    `;
+}
--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import {
   makeContactForm,
   setupContactForm,
 } from "./components/contact-form.js";
-
 import { makePortfolio } from "./components/portfolio.js";
+import { makeFooter } from "./components/footer.js";
 
 document.querySelector("#app").innerHTML = `
 <header id="header">
@@ -16,11 +16,13 @@ document.querySelector("#app").innerHTML = `
   <section id="portfolio"></section>
   <section id="contact"></section>
 </main>
+<footer id="footer">
+</footer
 `;
 
 makeHeader(document.querySelector("#header"));
 makeHero(document.querySelector("#hero"));
 makeContactForm(document.querySelector("#contact"));
 setupContactForm();
-
 makePortfolio(document.querySelector("#portfolio"));
+makeFooter(document.querySelector("#footer"));
