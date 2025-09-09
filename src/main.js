e948f2bbc05b04c96f0c73101f43d173a229d063
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 12:20:25 -0400

    drop the counter

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
 import { setupHeader } from "./components/header.js";
 
 document.querySelector("#app").innerHTML = `
 <header id="header">
 </header>
 <main>
-  <section class="hero">
+  <section id="hero">
     <div class="container">
       <h1>Welcome</h1>
-      <div class="card">
-        <button id="counter" type="button"></button>
-      </div>
     </div>
   </section>
 </main>
 `;
 
-setupCounter(document.querySelector("#counter"));
 setupHeader(document.querySelector("#header"));
