db10150322fd93b6c0e83f3d0ac8e9fc45aebfc0
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 12:03:25 -0400

    remove secondary nav list

--- src/main.js
+++ src/main.js
@@ -8,18 +8,10 @@ document.querySelector("#app").innerHTML = `
           <p>Josh Kaplan</p>
         </div>
         <nav class="nav">
-          <ul class="nav__list nav__list--primary">
+          <ul class="nav__list">
             <li class="nav__item"><a href="#" class="nav__link">Home</a></li>
             <li class="nav__item"><a href="#" class="nav__link">About</a></li>
             <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
-          </ul>
-          <ul class="nav__list nav__list--secondary">
-            <li class="nav__item">
-              <a href="#" class="nav__link">Sign In</a>
-            </li>
-            <li class="nav__item">
-              <a href="#" class="nav__link nav__link--button">Sign up</a>
-            </li>
           </ul>
         </nav>
       </div>
