e80b68138482b17d51da93e9e9f8fcb96318a09b
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-09 21:52:11 -0400

    add dark mode toggle button

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import "../styles/header.css";
+import lightModeImg from "../assets/images/light_mode.svg";
+import darkModeImg from "../assets/images/dark_mode.svg";
 
 export function makeHeader(element) {
   element.innerHTML = `
@@ -10,8 +12,10 @@ export function makeHeader(element) {
           <ul class="nav__list">
             <li class="nav__item"><a href="#portfolio" class="nav__link">Portfolio</a></li>
             <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+            <li class="nav__button"><button id="dark-mode-toggle"><img src="${darkModeImg}" alt="dark mode image"></button></li>
           </ul>
         </nav>
+        
       </div>
       `;
 }
