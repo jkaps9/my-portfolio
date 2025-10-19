2fd3a1d63d046aef9ce9ab0e03c2840346c82383
p10pro <15219491+jkaps9@users.noreply.github.com>
2025-10-19 13:30:01 -0400

    update hero copy

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,8 +3,8 @@ import "../styles/hero.css";
 export function makeHero(element) {
   element.innerHTML = `
     <div class="container">
-      <h1 class="hero__title">I craft digital experiences that captivate and convert</h1>
-      <p class="hero__content">I am a web developer creating beautiful and thoughtful websites. You work hard! I create websites that work hard for you so that you can focus on what you do best.</p>
+      <h1 class="hero__title">I build good websites</h1>
+      <p class="hero__content">I am a web developer creating beautiful and thoughtful websites. I create websites that work hard for you so that you can focus on what you do best.</p>
       <a href="#portfolio" class="hero__link">Explore portfolio</a>
     </div>
     `;
